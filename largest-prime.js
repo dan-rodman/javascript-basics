0c28aeff2f670bcf873a3331a4240f313327aa09
Dan Rodman <dan.rodman@outlook.com>
2017-05-08 15:27:36 -0700

    largest prime factor finder works for small numbers now

--- largest-prime.js
+++ largest-prime.js
@@ -16,6 +16,54 @@ var isPrime = function(x)
   	return true;
 };
 
+
+var getPrimes = function(y)
+{
+	var sieve = [];
+	var limit = y+1;
+	
+	for (i = 0; i < limit; i++)
+	{
+		sieve.push(i);
+	}
+	//console.log(sieve);
+
+  var x = 2;
+  var sifted = false;
+	
+	do{
+  for (p = x*x; p <= limit; p+=x)
+	{
+	  sieve[p] = 0;
+	}
+	//console.log(sieve);
+	x++; 
+	if (x >= limit)
+		{
+			sifted = true;
+		}
+	
+	}while(!sifted)
+	
+	for (j = limit; j >= 0; j--)
+	  {
+	    if (y % sieve[j] === 0)
+	    {
+	      return sieve[j];
+	    }
+	  }
+	return 0;
+};
+
+getPrimes(34);
+
+
+
+
+
+
+
+
 var getPrimes = function(y)
 {
 	var sieve = [];
